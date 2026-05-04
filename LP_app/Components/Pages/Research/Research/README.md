693f4a92af04b7b2c2711c6632b671da2867283e
ophelia labs <ophelialabs@gmail.com>
2026-05-04 17:05:17 -0400

    update

--- LP_app/Components/Pages/Research/Research/README.md
+++
@@ -1,3 +0,0 @@
-Research, Resources, Recent advances and applications, and current challenges. 
-
-Automatically update on a weekly basis
--- LP_app/obj/Debug/net10.0/LP_app.AssemblyInfo.cs
+++ LP_app/obj/Debug/net10.0/LP_app.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LP_app")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+04326cf3b65a00e54a85e0bfe4f5e6d06ef36c05")]
 [assembly: System.Reflection.AssemblyProductAttribute("LP_app")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LP_app")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
